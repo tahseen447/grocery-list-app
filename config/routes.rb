f3d114dbacf82f33e68611a09bd47d93cfa45436
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2019-02-06 23:39:32 +0000

    create new user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resources :users
+  root 'application#hello'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
