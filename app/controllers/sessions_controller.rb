3075ce237233c2c6e5b545844cb16f1376790ae7
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2019-02-07 17:53:06 +0000

    complete sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,10 @@
 class SessionsController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    binding.pry
     user = User.find_by(name: params[:user][:name])
     user = user.try(:authenticate, params[:user][:password])
     return redirect_to(controller: 'sessions', action: 'new') unless user
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,10 @@ class UsersController < ApplicationController
   end
 
   def create
-    binding.pry
-  @user = User.create(user_params)
-   return redirect_to :new unless @user.save
-   session[:user_id] = @user.id
-   redirect_to controller: 'application', action: 'hello'
+    @user = User.create(user_params)
+    return redirect_to :new unless @user.save
+    session[:user_id] = @user.id
+    redirect_to controller: 'application', action: 'hello'
   end
 
   def edit
