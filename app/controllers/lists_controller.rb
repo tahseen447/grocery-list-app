6431ddd3f7034748aceda5b82e68986a5584dc0f
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2019-02-08 23:20:20 +0000

    create, edit, update, destroy working for lists

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,9 @@
 class ListsController < ApplicationController
-  before_action :set_list, only: [:show, :edit, :update]
+  before_action :set_list, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @lists = List.all
+  end
 
   def new
     @list = List.new
@@ -17,11 +21,16 @@ class ListsController < ApplicationController
   end
 
   def update
-    if @list.update
+    if @list.update(list_params)
       redirect_to list_path(@list)
     else
       redirect_to edit_list_path(@list)
     end
+  end
+
+  def destroy
+    @list.destroy
+    redirect_to lists_path
   end
 
   private
