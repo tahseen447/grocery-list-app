abed538af97148d0de9572df90376a718ae2c5b1
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2019-02-08 01:21:22 +0000

    add validations

--- db/migrate/20190207214632_create_store_items.rb
+++ db/migrate/20190207214632_create_store_items.rb
@@ -1,9 +1,9 @@
 class CreateStoreItems < ActiveRecord::Migration[5.2]
   def change
     create_table :store_items do |t|
-      t.belongs_to :store, index: true, foreign_key: true
-      t.belongs_to :item, index: true, foreign_key: true
-      t.float :price
+      t.integer :store_id
+      t.integer :item_id
+      t.float :price, default=>0.0
       t.timestamps null: false
     end
   end
