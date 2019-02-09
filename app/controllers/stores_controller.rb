936d48d2ce5a34a49c8a96395195273e58fe7ec3
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2019-02-09 05:31:20 +0000

    add stores interface

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,2 +1,15 @@
 class StoresController < ApplicationController
+  before_action :set_store, only: [:show]
+
+  def index
+    @stores = Store.all
+  end
+
+  def show
+  end
+
+  private
+  def set_store
+    @store = Store.find(params[:id])
+  end
 end
