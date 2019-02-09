fd4c978bb1dcb275451d18e3721da47776d54422
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2019-02-09 08:15:35 +0000

    not working

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -6,6 +6,15 @@ class StoresController < ApplicationController
   end
 
   def show
+    if !params[:sort_order].blank?
+      if params[:sort_order] == "High to Low"
+        @store_items  = Store.items_high_to_low
+      elsif params[:sort_order] == "Low to High"
+        @store_items = Store.items_low_to_high
+      end
+    else
+      @store_items = Store.store_items
+    end
   end
 
   private
--- app/models/store.rb
+++ app/models/store.rb
@@ -1,4 +1,18 @@
 class Store < ApplicationRecord
   has_many :store_items
   has_many :items, through: :store_items
+
+
+  def self.items_high_to_low
+    store_items.order(price: :desc)
+  end
+
+  def self.items_low_to_high
+    store_items.order(price: :asc)
+  end
+
+  def self.store_items
+    binding.pry
+    store_items
+  end
 end
