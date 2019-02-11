7c4d28fd1419af9c72de9edb8d013839dea12110
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2019-02-11 23:40:45 +0000

    add navigation for user

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -14,8 +14,8 @@ class StoresController < ApplicationController
       end
     elsif !params[:list_id].blank?
       @store = Store.find(params[:store][:id])
-      list = List.find(params[:list_id])
-      list_items = list.items.all.map {|item| item.name}
+      @list = List.find(params[:list_id])
+      list_items = @list.items.all.map {|item| item.name}
       store_items = @store.store_items.all
       @store_items =store_items.map {|items| items if list_items.include?(items.item.name)}.compact
       else
